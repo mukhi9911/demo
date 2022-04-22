4bf7d581c542b58161418dddea47d101961dd719
mukhi9911 <59448414+mukhi9911@users.noreply.github.com>
2022-04-22 13:24:56 +0530

    Create HelloWorld.java

---
+++ EBSCustomizations/XXHR/java/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class HelloWorld
+{
+  public static void main(String args[])
+  {
+    int x=9;
+    System.out.println("pre increment: " + ++x);
+    System.out.println("post increment: " + x++);
+    System.out.println("pre decrement: " + --x);
+     System.out.println("post decrement: " + x--);
+     }
+     }
