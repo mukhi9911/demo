0aeebd8c49674920ca6171bf29345a6d023ab4a5
mukhi9911 <59448414+mukhi9911@users.noreply.github.com>
2022-04-22 13:31:19 +0530

    new file 1

---
+++ EBSCustomizations/XXHR/java/WorldHello.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class HelloWorld
+{
+  public static void main(String args[])
+  {
+    String a =new String("sri");
+    StringBuffer a1= new StringBuffer("Sri");
+    StringBuilder a2= new StringBuilder("SRI");
+    a.concat("mukhi");
+    a1.append("mukhi");
+    a2.append("mukhi");
+    System.out.println(a);
+    System.out.println(a1);
+    System.out.println(a2);
+    
+     }
+     }
